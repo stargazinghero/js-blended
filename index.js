5f34e75d4e10f91c1ee5c56d387c9711f3d5db17
Danndy6 <danndy754@gmail.com>
2021-10-24 13:20:29 +0300

    task_3

--- index.js
+++ index.js
@@ -25,12 +25,27 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
 // 70 === 01:10
 
-const globalMinutes = 122;
-const hours = Math.floor(globalMinutes / 60);
-const modifyHours = String(hours).padStart(2,0);
-const minutes = globalMinutes % 60;
-const modifyMinutes = String(minutes).padStart(2,0);
+// const globalMinutes = 122;
+// const hours = Math.floor(globalMinutes / 60);
+// const modifyHours = String(hours).padStart(2,0);
+// const minutes = globalMinutes % 60;
+// const modifyMinutes = String(minutes).padStart(2,0);
 
 
-// console.log(parseInt(hours));
-console.log(`${modifyHours}:${modifyMinutes}`);
+// // console.log(parseInt(hours));
+// console.log(`${modifyHours}:${modifyMinutes}`);
+ 
+//3. Напишите цыкл, который выводит в консоль
+//числа от max до min по убыванию
+// Выведите в консоль всех четных чисел от min до max
+const max = 50;
+const min = 23;
+let total = 0;
+
+for (let i = max; i >= min; i--) {
+    if (i % 2 === 0) {
+        continue
+    }
+    total += i; 
+}
+console.log(total);
