fad4150a27dd821223baac229919cc800ae9774c
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-10-24 13:46:22 +0300

    1

--- index.js
+++ index.js
@@ -38,14 +38,68 @@
 //3. Напишите цыкл, который выводит в консоль
 //числа от max до min по убыванию
 // Выведите в консоль всех четных чисел от min до max
-const max = 50;
-const min = 23;
-let total = 0;
+// const max = 50;
+// const min = 23;
+// let total = 0;
 
-for (let i = max; i >= min; i--) {
-    if (i % 2 === 0) {
-        continue
-    }
-    total += i; 
+// for (let i = max; i >= min; i--) {
+//     if (i % 2 === 0) {
+//         continue
+//     }
+//     total += i; 
+// }
+// console.log(total);
+
+//4. Напишите код, который будет спрашивать
+//логин с помощью prompt и логировать результат
+//в консоль браузера
+
+//Если посетитель вводит "Админ",
+//то prompt запрашивает пароль.
+//Если ничего не ввели или нажата клавиша Esc
+//вывести стороку "Отменено"
+//В противном случае вывксти строку "Я вас не знаю"
+
+//Пароль проверять так:
+//Если введён пароль "Я главный",
+//то вывести строку "Здравствуйте!"
+//иначе выводить строку "Неверный пароль!"
+
+const loginInput = prompt("Введите свой логин");
+const passworAdmin = "Админ";
+
+// if (loginInput === passworAdmin) {
+//     const passwordInput = prompt('Give me a Password');
+//     if (passwordInput === 'Я главный') {
+//         console.log("Здравствуйте!");
+//     } else if (passwordInput === null) {
+//         console.log("Отменено");
+//     } else {
+//         console.log("Неверный пароль!");
+//     }
+// }  else if(loginInput === null) {
+//     console.log("Отменено");
+// } else {
+//     console.log("Я вас не знаю");
+// }
+
+switch (loginInput) {
+    case "Админ":
+        console.log("Здравствуйте!");
+        const passwordInput = prompt('Give me a Password');
+        switch (passwordInput) {
+            case 'Я главный':
+                console.log("Здравствуйте!");
+                break
+            case null:
+                console.log("Отменено");
+            default:
+                console.log("Неверный пароль!");
+        }
+        break;
+    case null:
+        console.log("Отменено");
+        break;
+    default:
+        console.log("Я вас не знаю");
 }
-console.log(total);
