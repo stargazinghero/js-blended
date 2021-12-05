8b8f4389948b4fb13b415c1d0221f19d1ed6dae1
Kateryna Tolstykh <agrratt@gmail.com>
2021-12-05 14:04:55 +0200

    4

--- js/index.js
+++ js/index.js
@@ -1,16 +1,38 @@
 const keys = document.querySelectorAll('.key__item');
 
-const playSound = (event) => {
-    console.log(event.currentTarget.dataset.key);
-    const code = event.currentTarget.dataset.key;
+const play = (code) => {
     const audio = document.querySelector(`audio[data-key='${code}']`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
+}
+
+const playSound = (event) => {
+    
+    const code = event.currentTarget.dataset.key;
+    play(code);
     event.currentTarget.classList.add('playing')
-    console.log(audio);
+    
+}
+
+function removeTransition (event) {
+    if (event.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+    // console.log(this);
 }
   
 keys.forEach(btn => { btn.addEventListener('click', playSound) });
+keys.forEach(btn => 
+    btn.addEventListener('transitionend', removeTransition
+))
+window.addEventListener('keydown', keyboardPlaySound);
+
+function keyboardPlaySound(event) {
     
-    
+    const code = event.keyCode;
+    play(code);
+    // event.currentTarget.classList.add('playing')
+    const key = document.querySelector(`.key__item[data-key='${code}']`)
+    key.classList.add('playing');
+}
+
